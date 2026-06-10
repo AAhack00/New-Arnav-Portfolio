f5acaf11650650f83f6663d5bc0366e54214ae53
AAhack00 <aahacker000@gmail.com>
2026-06-10 16:54:45 +0530

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import "./styles/Loading.css";
 import { useLoading } from "../context/LoadingProvider";
-import { default as Marquee } from "react-fast-marquee";
 
 const Loading = ({ percent }: { percent: number }) => {
   const { setIsLoading } = useLoading();
